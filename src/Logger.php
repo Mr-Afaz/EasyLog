79cc2c1d8aa1dd93a5f93d5744ddc1eddd030f0c
MrAfaz <abolfazlmajidi100@gmail.com>
2023-10-27 01:02:44 +0330

    release 1.0.0

--- src/Logger.php
+++ src/Logger.php
@@ -1,46 +1,43 @@
 <?php
+
+
+/**
+ * This is a lightweight logger library for PHP, which allows developers to easily log messages at different levels such as error, warning, and info.
+ *
+ * @version 1.0.0
+ * @author Mr Afaz
+ * @package neili
+ * @copyright Copyright 2023 Neili library
+ * @license https://opensource.org/licenses/MIT
+ * @link https://github.com/imafaz/neili
+ */
+
+
+declare(strict_types=1);
+
 namespace EasyLog;
 
-/*
-*  Logger.php module
-*
-*  The MIT License (MIT)
-* 
-*  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-*  and associated documentation files (the "Software"), to deal in the Software without restriction,
-*  including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-*  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-*  subject to the following conditions:
-* 
-*  The above copyright notice and this permission notice shall be included in all copies or substantial
-*  portions of the Software.
-* 
-*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-*  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-*  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-*  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-*  @author Abolfazl Majidi (MrAfaz)
-*  @copyright Copyright (c) 2023 MrAfaz <mrafaz.com>
-*  @license https://opensource.org/licenses/MIT
-*/
-
-
-
-error_reporting(0);
-ini_set('display_errors', 0);
-
+use Exception;
 
 class Logger
 {
 
-    const ERROR = 'ERROR';
-    const WARNING = 'WARNING';
-    const INFO = 'INFO';
 
-    
-    
-    
+    /**
+     * log level types
+     *
+     * @constant int
+     */
+    const DEBUG = 1;
+    const INFO = 2;
+    const WARNING = 3;
+    const ERROR = 4;
+    const FATAL = 5;
+
+
+
+
+
     /**
      * logFile
      *
@@ -48,16 +45,16 @@ class Logger
      */
     private $logFile;
 
-  
-    
+
+
     /**
      * printLog
      *
      * @var mixed
      */
-    private $printLog;
-    
-    
+    public $printLog;
+
+
     /**
      * __construct: setup logger
      *
@@ -65,63 +62,104 @@ class Logger
      * @param  mixed $printLog
      * @return void
      */
-    public function __construct(string $logFile,bool $printLog = false){
-        $this->logFile = $logFile;
-        $this->printLog = $printLog;
-    }
-    
-    
-    /**
-     * log: write and print logs
-     *
-     * @param  mixed $message
-     * @param  mixed $level
-     * @return void
-     */
-    public function log(string $message, string $level = self::INFO): void
+    public function __construct(string $logFile, bool $printLog = false)
     {
-        $log = sprintf("[%s] [%s] %s\n", date('Y-m-d H:i:s'),$level, $message);
-        file_put_contents($this->logFile, $log, FILE_APPEND);
-
-        if ($this->printLog) {
-            echo $log;
+        $this->printLog = $printLog;
+        $this->logFile = $logFile;
+        if (function_exists('ini_set')) {
+            ini_set('log_errors', '1');
+            ini_set('error_log', $logFile);
         }
     }
-    
-    
+
+
+
     /**
-     * error: log error
+     * writing/print log
      *
-     * @param  mixed $message
+     * @param string $message
+     * @param int $level
      * @return void
      */
-    public function error(string $message): void
+    private function logging(string $message, int $level): void
     {
-        $this->log($message, self::ERROR);
+        $date = gmdate('d-M-Y H:i:s \U\T\C');
+        if ($level == self::DEBUG) {
+            $log = sprintf("[%s] [%s] %s\n", $date, 'DEBUG', $message);
+        } elseif ($level == self::INFO) {
+            $log = sprintf("[%s] [%s] %s\n", $date, 'INFO', $message);
+        } elseif ($level == self::WARNING) {
+            $log = sprintf("[%s] [%s] %s\n", $date, 'WARNING', $message);
+        } elseif ($level == self::ERROR) {
+            $log = sprintf("[%s] [%s] %s\n", $date, 'ERROR', $message);
+        } elseif ($level == self::FATAL) {
+            $log = sprintf("[%s] [%s] %s\n", $date, 'FATAL ERROR', $message);
+            throw new Exception($message);
+        } else {
+            throw new Exception('invalid level type!');
+        }
+        if ($this->printLog) {
+            print($log);
+        }
+        file_put_contents($this->logFile, $log, FILE_APPEND);
     }
-    
-    
+
+
     /**
-     * warning: log warning
+     * logging debug
      *
-     * @param  mixed $message
+     * @param  string $message
      * @return void
      */
-    public function warning(string $message): void
+    public function debug(string $message): void
     {
-        $this->log($message, self::WARNING);
+        $this->logging($message, self::DEBUG);
     }
-    
-    
+
+
+
     /**
-     * info: log info
+     * logging info
      *
-     * @param  mixed $message
+     * @param  string $message
      * @return void
      */
     public function info(string $message): void
     {
-        $this->log($message, self::INFO);
+        $this->logging($message, self::INFO);
     }
-    
+
+    /**
+     * logging warning
+     *
+     * @param  string $message
+     * @return void
+     */
+    public function warning(string $message): void
+    {
+        $this->logging($message, self::WARNING);
+    }
+
+
+    /**
+     * logging error
+     *
+     * @param  string $message
+     * @return void
+     */
+    public function error(string $message): void
+    {
+        $this->logging($message, self::ERROR);
+    }
+
+    /**
+     * fatal error
+     *
+     * @param  string $message
+     * @return void
+     */
+    public function fatal(string $message): void
+    {
+        $this->logging($message, self::FATAL);
+    }
 }
--- tests/usage.php
+++ tests/usage.php
@@ -1,18 +1,18 @@
 <?php
 
-
 require_once('..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Logger.php');
 
 use EasyLog\Logger;
 
 // create a new logger with the file name "projectName.log"
-$log = new Logger('projectName.log',true);
+$log = new Logger('projectName.log', true);
 
-// log an error message
-$log->error('An error occurred!');
+$log->debug('This is a debug message.');
 
-// log a warning message
-$log->warning('This is a warning!');
+$log->info('This is an informational message.');
 
-// log an informational message
-$log->info('Some information for you!');
+$log->warning('This is a warning message.');
+
+$log->error('This is an error message.');
+
+$log->fatal('This is a fatal message.');
